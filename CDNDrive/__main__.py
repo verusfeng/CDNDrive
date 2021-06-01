1f960282502ba67504b07e9c298a9775c68f2cd5
vonvin <1@1.com>
2021-06-01 23:08:36 +0800

    Set default thread cnt as 8

--- CDNDrive/__main__.py
+++ CDNDrive/__main__.py
@@ -30,6 +30,7 @@ succ = True
 nblocks = 0
 lock = threading.Lock()
 
+
 def load_api_by_prefix(s):
     global api
     global encoder
@@ -42,49 +43,63 @@ def load_api_by_prefix(s):
     encoder = encoders[site]
     return True
 
+
 def fetch_meta(s):
     url = api.meta2real(s)
-    if not url: return None
+    if not url:
+        return None
     full_meta = api.image_download(url)
-    if not full_meta: return None
+    if not full_meta:
+        return None
     meta_dict = json.loads(encoder.decode(full_meta).decode("utf-8"))
     return meta_dict
 
+
 def login_handle(args):
     global api
-    
+
     api = drivers[args.site]
     r = api.login(args.username, args.password)
     if r['code'] != 0:
         log(f"登录失败：{r['message']}")
         return
     info = api.get_user_info()
-    if info: log(info)
-    else: log("用户信息获取失败")
+    if info:
+        log(info)
+    else:
+        log("用户信息获取失败")
+
 
 def cookies_handle(args):
     global api
-    
+
     api = drivers[args.site]
     api.set_cookies(args.cookies)
     info = api.get_user_info()
-    if info: log(info)
-    else: log("用户信息获取失败")
+    if info:
+        log(info)
+    else:
+        log("用户信息获取失败")
+
 
 def userinfo_handle(args):
     global api
-    
+
     api = drivers[args.site]
     info = api.get_user_info()
-    if info: log(info)
-    else: log("用户未登录")
-    
+    if info:
+        log(info)
+    else:
+        log("用户未登录")
+
+
 def tr_upload(i, block, block_dict):
     global succ
 
     enco_block = encoder.encode(block)
     for j in range(10):
-        if not succ: break
+        if not succ:
+            break
         r = api.image_upload(enco_block)
         if r['code'] == 0:
             block_dict.update({
@@ -96,8 +111,10 @@ def tr_upload(i, block, block_dict):
             break
         else:
             log(f"分块{i + 1}/{nblocks}第{j + 1}次上传失败：{r.get('message')}")
-            if j == 9: succ = False
-    
+            if j == 9:
+                succ = False
+
+
 def upload_handle(args):
     global api
     global encoder
@@ -108,7 +125,7 @@ def upload_handle(args):
     encoder = encoders[args.site]
     start_time = time.time()
     file_name = args.file
-    
+
     if not path.exists(file_name):
         log(f"文件{file_name}不存在")
         return
@@ -116,36 +133,42 @@ def upload_handle(args):
         log("暂不支持上传文件夹")
         return
     log(f"上传: {path.basename(file_name)} ({size_string(path.getsize(file_name))})")
-    first_4mb_sha1 = calc_sha1(read_in_chunk(file_name, size=4 * 1024 * 1024, cnt=1))
+    first_4mb_sha1 = calc_sha1(read_in_chunk(
+        file_name, size=4 * 1024 * 1024, cnt=1))
     history = read_history(args.site)
     if first_4mb_sha1 in history:
         url = history[first_4mb_sha1]['url']
-        log(f"文件已于{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(history[first_4mb_sha1]['time']))}上传, 共有{len(history[first_4mb_sha1]['block'])}个分块")
+        log(
+            f"文件已于{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(history[first_4mb_sha1]['time']))}上传, 共有{len(history[first_4mb_sha1]['block'])}个分块")
         log(f"META URL -> {api.real2meta(url)}")
         return url
 
     if not api.get_user_info():
         log("账号未登录，请先登录")
         return
-        
+
     log(f"线程数: {args.thread}")
     succ = True
-    nblocks = math.ceil(path.getsize(file_name) / (args.block_size * 1024 * 1024))
+    nblocks = math.ceil(path.getsize(file_name) /
+                        (args.block_size * 1024 * 1024))
     block_dicts = [{} for _ in range(nblocks)]
     trpool = ThreadPoolExecutor(args.thread)
     hdls = []
-    
+
     blocks = read_in_chunk(file_name, size=args.block_size * 1024 * 1024)
     for i, block in enumerate(blocks):
         hdl = trpool.submit(tr_upload, i, block, block_dicts[i])
         hdls.append(hdl)
         # 及时清理队列中的任务
-        if len(hdls) == args.thread:    
-            for h in hdls: h.result()
+        if len(hdls) == args.thread:
+            for h in hdls:
+                h.result()
             hdls = []
-    for h in hdls: h.result()
-    if not succ: return
-    
+    for h in hdls:
+        h.result()
+    if not succ:
+        return
+
     sha1 = calc_sha1(read_in_chunk(file_name))
     meta_dict = {
         'time': int(time.time()),
@@ -162,7 +185,9 @@ def upload_handle(args):
         log("元数据上传完毕")
         log(f"{meta_dict['filename']} ({size_string(meta_dict['size'])}) 上传完毕, 用时{time.time() - start_time:.1f}秒, 平均速度{size_string(meta_dict['size'] / (time.time() - start_time))}/s")
         log(f"META URL -> {api.real2meta(url)}")
+        log(f"WEB  URL -> {url}")
         write_history(first_4mb_sha1, meta_dict, args.site, url)
+        # TBD write sql here ...
         return url
     else:
         log(f"元数据上传失败：{r.get('message')}")
@@ -174,11 +199,13 @@ def tr_download(i, block_dict, f, offset):
 
     url = block_dict['url']
     for j in range(10):
-        if not succ: break
+        if not succ:
+            break
         block = api.image_download(url)
         if not block:
             log(f"分块{i + 1}/{nblocks}第{j + 1}次下载失败")
-            if j == 9: succ = False
+            if j == 9:
+                succ = False
             continue
         block = encoder.decode(block)
         if calc_sha1(block) == block_dict['sha1']:
@@ -189,8 +216,9 @@ def tr_download(i, block_dict, f, offset):
             break
         else:
             log(f"分块{i + 1}/{nblocks}校验未通过")
-            if j == 9: succ = False
-            
+            if j == 9:
+                succ = False
+
 
 def download_handle(args):
     global succ
@@ -220,7 +248,7 @@ def download_handle(args):
     nblocks = len(meta_dict['block'])
     trpool = ThreadPoolExecutor(args.thread)
     hdls = []
-    
+
     mode = "r+b" if path.exists(file_name) else "wb"
     with open(file_name, mode) as f:
         for i, block_dict in enumerate(meta_dict['block']):
@@ -228,13 +256,16 @@ def download_handle(args):
             hdl = trpool.submit(tr_download, i, block_dict, f, offset)
             hdls.append(hdl)
             # 及时清理队列中的任务
-            if len(hdls) == args.thread:    
-                for h in hdls: h.result()
+            if len(hdls) == args.thread:
+                for h in hdls:
+                    h.result()
                 hdls = []
-        for h in hdls: h.result()
-        if not succ: return
+        for h in hdls:
+            h.result()
+        if not succ:
+            return
         f.truncate(meta_dict['size'])
-    
+
     log(f"{path.basename(file_name)} ({size_string(meta_dict['size'])}) 下载完毕, 用时{time.time() - start_time:.1f}秒, 平均速度{size_string(meta_dict['size'] / (time.time() - start_time))}/s")
     sha1 = calc_sha1(read_in_chunk(file_name))
     if sha1 == meta_dict['sha1']:
@@ -243,6 +274,7 @@ def download_handle(args):
     else:
         log("文件校验未通过")
         return
+
 
 def info_handle(args):
     if not load_api_by_prefix(args.meta):
@@ -254,10 +286,11 @@ def info_handle(args):
     else:
         log("元数据解析失败")
 
+
 def history_handle(args):
     global api
 
-    from .load_sql import load_history_to_sql_db as lll 
+    from .load_sql import load_history_to_sql_db as lll
     all_history = read_history()
     if len(all_history) == 0:
         print(f"暂无历史记录")
@@ -277,10 +310,14 @@ def history_handle(args):
             lll(meta_dict)
             idx += 1
 
+
 def interact_mode(parser, subparsers):
-    subparsers.add_parser("help", help="show this help message").set_defaults(func=lambda _: parser.parse_args(["--help"]).func())
-    subparsers.add_parser("version", help="show program's version number").set_defaults(func=lambda _: parser.parse_args(["--version"]).func())
-    subparsers.add_parser("exit", help="exit program").set_defaults(func=lambda _: os._exit(0))
+    subparsers.add_parser("help", help="show this help message").set_defaults(
+        func=lambda _: parser.parse_args(["--help"]).func())
+    subparsers.add_parser("version", help="show program's version number").set_defaults(
+        func=lambda _: parser.parse_args(["--version"]).func())
+    subparsers.add_parser("exit", help="exit program").set_defaults(
+        func=lambda _: os._exit(0))
     parser.print_help()
     while True:
         try:
@@ -290,20 +327,24 @@ def interact_mode(parser, subparsers):
         except:
             pass
 
+
 def main():
     signal.signal(signal.SIGINT, lambda signum, frame: os.kill(os.getpid(), 9))
-    parser = argparse.ArgumentParser(prog="CDNDrive", description="Make Picbeds Great Cloud Storages!", formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-v", "--version", action="version", version=f"CDNDrive version: {__version__}")
+    parser = argparse.ArgumentParser(
+        prog="CDNDrive", description="Make Picbeds Great Cloud Storages!", formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-v", "--version", action="version",
+                        version=f"CDNDrive version: {__version__}")
     parser.set_defaults(func=lambda x: parser.print_help())
     subparsers = parser.add_subparsers()
-    
+
     login_parser = subparsers.add_parser("login", help="log in to the site")
     login_parser.add_argument("site", help="site", choices=drivers.keys())
     login_parser.add_argument("username", help="username")
     login_parser.add_argument("password", help="password")
     login_parser.set_defaults(func=login_handle)
-    
-    cookies_parser = subparsers.add_parser("cookies", help="set cookies to the site")
+
+    cookies_parser = subparsers.add_parser(
+        "cookies", help="set cookies to the site")
     cookies_parser.add_argument("site", help="site", choices=drivers.keys())
     cookies_parser.add_argument("cookies", help="cookies")
     cookies_parser.set_defaults(func=cookies_handle)
@@ -311,29 +352,36 @@ def main():
     userinfo_parser = subparsers.add_parser("userinfo", help="get userinfo")
     userinfo_parser.add_argument("site", help="site", choices=drivers.keys())
     userinfo_parser.set_defaults(func=userinfo_handle)
-    
+
     upload_parser = subparsers.add_parser("upload", help="upload a file")
     upload_parser.add_argument("site", help="site", choices=drivers.keys())
     upload_parser.add_argument("file", help="name of the file to upload")
-    upload_parser.add_argument("-b", "--block-size", default=4, type=int, help="block size in MB")
-    upload_parser.add_argument("-t", "--thread", default=4, type=int, help="upload thread number")
+    upload_parser.add_argument(
+        "-b", "--block-size", default=4, type=int, help="block size in MB")
+    upload_parser.add_argument(
+        "-t", "--thread", default=8, type=int, help="upload thread number")
     upload_parser.set_defaults(func=upload_handle)
-    
+
     download_parser = subparsers.add_parser("download", help="download a file")
     download_parser.add_argument("meta", help="meta url")
-    download_parser.add_argument("file", nargs="?", default="", help="new file name")
-    download_parser.add_argument("-f", "--force", action="store_true", help="force to overwrite if file exists")
-    download_parser.add_argument("-t", "--thread", default=8, type=int, help="download thread number")
+    download_parser.add_argument(
+        "file", nargs="?", default="", help="new file name")
+    download_parser.add_argument(
+        "-f", "--force", action="store_true", help="force to overwrite if file exists")
+    download_parser.add_argument(
+        "-t", "--thread", default=8, type=int, help="download thread number")
     download_parser.set_defaults(func=download_handle)
-    
+
     info_parser = subparsers.add_parser("info", help="show meta info")
     info_parser.add_argument("meta", help="meta url")
     info_parser.set_defaults(func=info_handle)
-    history_parser = subparsers.add_parser("history", help="show upload history")
+    history_parser = subparsers.add_parser(
+        "history", help="show upload history")
     history_parser.set_defaults(func=history_handle)
-    
+
     args = parser.parse_args()
     args.func(args)
+
 
 if __name__ == "__main__":
     main()
--- CDNDrive/load_sql.py
+++ CDNDrive/load_sql.py
@@ -9,11 +9,16 @@ def load_history_to_sql_db(metadict):
  'url': 'bdex://17e543b86c128e7a68f477ccf2649ed7d9e81c93'}
     '''
 
-    import os , sqlite3,logging ,json ,base64
-    logging.basicConfig(level = 0 )
-    if os.getenv("COMPUTERNAME") == "LAPTOP-IJTKOVMF":  # my acer laptop.. 
-        # sqlite3.db -> bilidrive 
-        db_url = [each for each in os.getenv("database").split(";") if "sqlite3.db" in each][0].strip()
+    import os
+    import sqlite3
+    import logging
+    import json
+    import base64
+    logging.basicConfig(level=0)
+    if os.getenv("COMPUTERNAME") == "LAPTOP-IJTKOVMF":  # my acer laptop..
+        # sqlite3.db -> bilidrive
+        db_url = [each for each in os.getenv("database").split(
+            ";") if "sqlite3.db" in each][0].strip()
         logging.info(db_url)
         with sqlite3.connect(db_url) as db:
             # create_table_sql = '''
@@ -23,7 +28,7 @@ def load_history_to_sql_db(metadict):
             # upload_time timestamp NOT NULL,
             # filename    text NOT NULL,
             # size        BIGINT NOT NULL,
-            # url         TEXT   NOT NULL, 
+            # url         TEXT   NOT NULL,
             # block_list_base64  TEXT NOT NULL
             # );'''
             # db.execute(create_table_sql)
@@ -34,16 +39,18 @@ def load_history_to_sql_db(metadict):
             '''
             sha1 = metadict["sha1"]
             upload_time = metadict["time"]
-            filename = metadict["filename"].replace("'","''")
+            filename = metadict["filename"].replace("'", "''")
             size = metadict["size"]
             url = metadict["url"]
-            block_list_base64 = base64.b64encode( json.dumps(metadict["block"]).encode() ).decode()
+            block_list_base64 = base64.b64encode(
+                json.dumps(metadict["block"]).encode()).decode()
 
-            string =  replace_sql.format(sha1= sha1, upload_time = upload_time, filename = filename , size = size , url = url , block_list_base64 = block_list_base64)
+            string = replace_sql.format(sha1=sha1, upload_time=upload_time, filename=filename,
+                                        size=size, url=url, block_list_base64=block_list_base64)
             db.execute(string)
             db.commit()
         # db.close()
 
-    # end if 
+    # end if
 
 # end def load_history...
